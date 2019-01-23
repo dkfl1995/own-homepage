7e086de4f367cb23b15ccb5c172acd3bf3269558
dkfl1995 <vlaio.95@gmail.com>
2019-01-23 15:23:33 +0200

    Updates

---
+++ precache-manifest.87343ac4e462bd7a04d01cfa039f43f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_homepage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "171cd71e09f8a3d29825",
+    "url": "/react_homepage/static/js/runtime~main.171cd71e.js"
+  },
+  {
+    "revision": "5c025eca4bce4bb7ee79",
+    "url": "/react_homepage/static/js/main.5c025eca.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react_homepage/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5c025eca4bce4bb7ee79",
+    "url": "/react_homepage/static/css/main.0f2c3bf3.chunk.css"
+  },
+  {
+    "revision": "cd314816b836108a5a69cb1b10e2842b",
+    "url": "/react_homepage/index.html"
+  }
+];
